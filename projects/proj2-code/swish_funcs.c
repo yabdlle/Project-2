533d80875e78b669e2d60d889ff5eaed40a31245
Aman Has <atallaabo@gmail.com>
2025-03-20 23:39:16 +0000

    added comments and error checks to main and some functions

--- projects/proj2-code/swish_funcs.c
+++ projects/proj2-code/swish_funcs.c
@@ -121,23 +121,6 @@ int run_command(strvec_t *tokens) {
 }
 
 int resume_job(strvec_t *tokens, job_list_t *jobs, int is_foreground) {
-    // TODO Task 5: Implement the ability to resume stopped jobs in the foreground
-    // 1. Look up the relevant job information (in a job_t) from the jobs list
-    //    using the index supplied by the user (in tokens index 1)
-    //    Feel free to use sscanf() or atoi() to convert this string to an int
-    // 2. Call tcsetpgrp(STDIN_FILENO, <job_pid>) where job_pid is the job's process ID
-    // 3. Send the process the SIGCONT signal with the kill() system call
-    // 4. Use the same waitpid() logic as in main -- don't forget WUNTRACED
-    // 5. If the job has terminated (not stopped), remove it from the 'jobs' list
-    // 6. Call tcsetpgrp(STDIN_FILENO, <shell_pid>). shell_pid is the *current*
-    //    process's pid, since we call this function from the main shell process
-
-    // TODO Task 6: Implement the ability to resume stopped jobs in the background.
-    // This really just means omitting some of the steps used to resume a job in the foreground:
-    // 1. DO NOT call tcsetpgrp() to manipulate foreground/background terminal process group
-    // 2. DO NOT call waitpid() to wait on the job
-    // 3. Make sure to modify the 'status' field of the relevant job list entry to BACKGROUND
-    //    (as it was STOPPED before this)
 
     // checking if the correct number of tokens is provided
     if (tokens->length < 2) {
@@ -161,6 +144,7 @@ int resume_job(strvec_t *tokens, job_list_t *jobs, int is_foreground) {
 
     // getting process group ID of job
     pid_t job_pgid = job->pid;
+
     // resume job in the foreground
     if (is_foreground) {
         // I need to move job's process group to forground
@@ -214,30 +198,30 @@ int resume_job(strvec_t *tokens, job_list_t *jobs, int is_foreground) {
 }
 
 int await_background_job(strvec_t *tokens, job_list_t *jobs) {
-    // TODO Task 6: Wait for a specific job to stop or terminate
-    // 1. Look up the relevant job information (in a job_t) from the jobs list
-    //    using the index supplied by the user (in tokens index 1)
-    // 2. Make sure the job's status is BACKGROUND (no sense waiting for a stopped job)
-    // 3. Use waitpid() to wait for the job to terminate, as you have in resume_job() and main().
-    // 4. If the process terminates (is not stopped by a signal) remove it from the jobs list
+
+    // Ensuring tokens and jobs pointers are valid
+    if (tokens == NULL || jobs == NULL) {
+        fprintf(stderr, "await_background_job: Invalid parameters (NULL pointer)\n");
+        return -1;
+    }
 
     // checking enough tokens
     if (tokens->length < 2) {
-        fprintf(stderr, "tokens length error");
+        fprintf(stderr, "Missing job index for await_background_job");
         return -1;
     }
 
     // parsing job index from second token
     int job_ind;
     if (sscanf(tokens->data[1], "%d", &job_ind) != 1) {
-        fprintf(stderr, "Invalid job index.");
+        fprintf(stderr, "Invalid job index\n");
         return -1;
     }
 
     // retrieving job from job list
     job_t *job = job_list_get(jobs, job_ind);
     if (job == NULL) {
-        fprintf(stderr, "Job index out of bounds.");
+        fprintf(stderr, "Job index out of bounds\n");
         return -1;
     }
 
